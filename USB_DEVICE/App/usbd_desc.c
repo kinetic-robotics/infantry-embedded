4b4ef6b06a2aacf3c31ce199b54e2139b18305c5
xqe2011 <gz923553148@gmail.com>
2020-11-26 12:39:15 +0800

    删除DCT与USB代码 支持SWV调试 删除不必要文件

--- USB_DEVICE/App/usbd_desc.c
+++ USB_DEVICE/App/usbd_desc.c
@@ -63,11 +63,11 @@
   * @{
   */
 
-#define USBD_VID     9235
+#define USBD_VID     1155
 #define USBD_LANGID_STRING     1033
 #define USBD_MANUFACTURER_STRING     "KineticRobotics"
-#define USBD_PID_FS     1000
-#define USBD_PRODUCT_STRING_FS     "Debug And Configuration Tool"
+#define USBD_PID_FS     22336
+#define USBD_PRODUCT_STRING_FS     "Debug And Configuration Virual Port"
 #define USBD_CONFIGURATION_STRING_FS     "CDC Config"
 #define USBD_INTERFACE_STRING_FS     "CDC Interface"
 
